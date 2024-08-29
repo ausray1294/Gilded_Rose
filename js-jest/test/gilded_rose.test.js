b9fc0934b2848a91939eaee0d021567534bd3500
ausray1294 <ausray1294@gmail.com>
2024-08-28 18:05:24 -0600

    add test for array initilization

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -2,8 +2,15 @@ const { Item, Shop } = require('../src/gilded_rose.js');
 // You need more tests than just the ones written here, this is just to get you started.
 // USE COVERAGE GUTTERS TO GUIDE YOUR TEST WRITING
 describe('Gilded Rose Pin Down Tests', () => {
-  describe('Items should be an array', () => {});
-  describe('should accurately update the normal items', () => {
+  describe('Items should be an array', () => {
+    test('It should initialize an empty array', () => {
+      const shop = new Shop();
+      expect(shop.items).toBeInstanceOf(Array);
+      expect(shop.items.length).toBe(0);
+    });
+  });
+
+  describe('It should accurately update the normal items when updateNormalItem is called', () => {
     test('Normal items should degrade in quality by 1 each day', () => {
       let normalItem = new Item('normal', 10, 20); //build
       const gildedRose = new Shop([normalItem]);
@@ -34,7 +41,7 @@ describe('Gilded Rose Pin Down Tests', () => {
     });
   });
 
-  describe('should accurately update "Aged Brie" when updateAgedBrie is called', () => {
+  describe('It should accurately update "Aged Brie" when updateAgedBrie is called', () => {
     test('Quality of "Aged Brie" should 50 when function updateAgedBrie is called', () => {
       let agedBrie = new Item('Aged Brie', 5, 50);
       const gildedRose = new Shop([agedBrie]);
@@ -64,7 +71,7 @@ describe('Gilded Rose Pin Down Tests', () => {
     });
   });
 
-  describe("should accurately update the 'Backstage Passes' when updateBackstagePasses is called", () => {
+  describe("It should accurately update the 'Backstage Passes' when updateBackstagePasses is called", () => {
     test('Quality of "Backstage passes" should increase by 3 when there are 5 days or less', () => {
       let backstagePass = new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20);
       const gildedRose = new Shop([backstagePass]);
@@ -72,6 +79,7 @@ describe('Gilded Rose Pin Down Tests', () => {
       const items = gildedRose.updateQuality();
 
       expect(items[0].quality).toBe(23);
+      expect(items[0].name).not.toEqual('Sulfuras, Hand of Ragnaros');
     });
 
     test('Quality of "Backstage passes" should increase by 2 when there are 10 days or less', () => {
@@ -102,7 +110,7 @@ describe('Gilded Rose Pin Down Tests', () => {
     });
   });
 
-  describe("should not do anything to 'Sulfuras' when checkNameFunction is called", () => {
+  describe("It should not do anything to 'Sulfuras' when checkNameFunction is called", () => {
     test('Item, Sulfuras, Hand of Ragnaros, should not decrease in quality', () => {
       let sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
       const gildedRose = new Shop([sulfuras]);
