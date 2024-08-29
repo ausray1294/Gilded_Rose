0a4698d333584f36f440994cbe48c39dd532d8db
ausray1294 <ausray1294@gmail.com>
2024-08-29 08:48:18 -0600

    add additional checks to sellIn and quality ternary operators

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -28,14 +28,14 @@ class Shop {
   updateNormalItem = (item) => {
     item.sellIn -= 1;
     item.quality = item.quality > 0 ? item.quality - 1 : item.quality;
-    item.quality = item.sellIn < 0 && item.quality > 0 ? item.quality - 1 : item.quality;
+    item.quality = item.sellIn <= 0 && item.quality > 0 ? item.quality - 1 : item.quality;
     return item;
   };
 
   updateAgedBrie = (item) => {
     item.sellIn -= 1;
     item.quality = item.quality < 50 ? item.quality + 1 : item.quality;
-    item.quality = item.sellIn < 0 && item.quality < 50 ? item.quality + 1 : item.quality;
+    item.quality = item.sellIn <= 0 && item.quality <= 50 ? item.quality + 1 : item.quality;
     return item;
   };
 
